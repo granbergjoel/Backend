67f8375c22a34f8781a4143f2b8b021fd45d5e35
Joel Granberg <granberg.joel@gmail.com>
2021-01-04 11:35:08 +0100

    första lektion

---
+++ Lektion 01/exempel-01.php
@@ -0,0 +1,22 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+
+
+    <h1>Exempel - 01</h1>
+    <?php 
+    
+    //här skrivs php-koden
+    echo "Hello World!";
+
+
+    ?> 
+    </body>
+    </html>
+    
+    
