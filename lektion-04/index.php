83b84675d01cd2f2e2a0887fa58a531c37eaa8d6
Joel Granberg <granberg.joel@gmail.com>
2021-01-14 09:52:28 +0100

    lektion 4, gjort en lista

---
+++ lektion-04/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+    include ("App.php"); 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>10 namn från <a href="https://namnapi.se">www.namnapi.se</a></h1>
+
+<?php
+
+    App::main();
+
+?>
+
+</body>
+</html>
