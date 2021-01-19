6cf13f3cfa0c81d950934f34e15d194a36244f4e
Joel Granberg <granberg.joel@gmail.com>
2021-01-19 09:20:55 +0100

    hmm

--- uppgift-01/del1.php
+++ uppgift-01/del1.php
@@ -4,6 +4,6 @@ date_default_timezone_set("Europe/Stockholm");
 $days= array("null","Måndag", "Tisdag","Onsdag", "Torsdag", "Fredag","Lördag","Söndag");
 $months = array("null","januari", "februari", "mars","april","maj","juni","juli","augusti","september","oktober","november","december");
 
-echo "Vecka ". date("w "  ). "- " . $days[date("w")]. " den " .date("d ").$months[date("n")].date(" Y"). " kl. ". date("H:i");
+echo "Vecka ". date("W "  ). "- " . $days[date("w")]. " den " .date("d ").$months[date("n")].date(" Y"). " kl. ". date("H:i");
 
 ?>
